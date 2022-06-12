26d2258760b06a131d3355da95425f693b938dca
s1936914629 <1936914629@qq.com>
2022-06-12 22:43:00 +0800

    修复update_user为空

--- src/main/java/cn/org/sqx/ReggieApplication.java
+++ src/main/java/cn/org/sqx/ReggieApplication.java
@@ -3,12 +3,16 @@ package cn.org.sqx;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author wff
  */
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
+@EnableTransactionManagement
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class, args);
