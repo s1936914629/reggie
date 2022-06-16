86b2a55538d1b8db3615783d5a4843709c99b382
s1936914629 <1936914629@qq.com>
2022-06-16 16:39:09 +0800

    套餐管理完成

--- src/main/java/cn/org/sqx/reggie/controller/SetmealController.java
+++ src/main/java/cn/org/sqx/reggie/controller/SetmealController.java
@@ -2,15 +2,20 @@ package cn.org.sqx.reggie.controller;
 
 import cn.org.sqx.reggie.common.R;
 import cn.org.sqx.reggie.dto.SetmealDto;
+import cn.org.sqx.reggie.entity.Category;
+import cn.org.sqx.reggie.entity.Setmeal;
 import cn.org.sqx.reggie.service.CategoryService;
 import cn.org.sqx.reggie.service.SetmealDishService;
 import cn.org.sqx.reggie.service.SetmealService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 套餐管理
@@ -43,4 +48,64 @@ public class SetmealController {
 
         return R.success("新增套餐成功");
     }
+
+    /**
+     * 套餐分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        //分页构造器
+        Page<Setmeal> pageInfo = new Page<>(page, pageSize);
+        Page<SetmealDto> dtoPage = new Page<>();
+
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        //添加查询条件，根据name进行like模糊查询
+        queryWrapper.like(name != null, Setmeal::getName, name);
+        //添加排序天剑，根据更新时间降序排列
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+
+        setmealService.page(pageInfo, queryWrapper);
+
+        //对象拷贝
+        BeanUtils.copyProperties(pageInfo, dtoPage, "records");
+        List<Setmeal> records = pageInfo.getRecords();
+
+        List<SetmealDto> list=records.stream().map((item)->{
+            SetmealDto setmealDto = new SetmealDto();
+            //对象拷贝
+            BeanUtils.copyProperties(item,setmealDto);
+            //分类id
+            Long categoryId = item.getCategoryId();
+            //根据分类id查询分类对象
+            Category category = categoryService.getById(categoryId);
+            if(category!=null){
+                //分类名称
+                String categoryName = category.getName();
+                setmealDto.setCategoryName(categoryName);
+            }
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        dtoPage.setRecords(list);
+        return R.success(dtoPage);
+    }
+
+    /**
+     * 删除套餐
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    public R<String> delete(@RequestParam List<Long> ids) {
+        log.info("ids:{}",ids);
+
+        setmealService.removeWithDish(ids);
+
+        return R.success("套餐数据删除成功");
+    }
+
 }
--- src/main/java/cn/org/sqx/reggie/service/impl/SetmealServiceImpl.java
+++ src/main/java/cn/org/sqx/reggie/service/impl/SetmealServiceImpl.java
@@ -1,13 +1,16 @@
 package cn.org.sqx.reggie.service.impl;
 
+import cn.org.sqx.reggie.common.CustomException;
 import cn.org.sqx.reggie.dto.SetmealDto;
 import cn.org.sqx.reggie.entity.Setmeal;
 import cn.org.sqx.reggie.entity.SetmealDish;
 import cn.org.sqx.reggie.mapper.SetmealMapper;
 import cn.org.sqx.reggie.service.SetmealDishService;
 import cn.org.sqx.reggie.service.SetmealService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,6 +31,7 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
     @Override
     @Transactional
     public void saveWithDish(SetmealDto setmealDto) {
+
         //保存套餐的基本信息，操作setmeal，执行insert操作
         this.save(setmealDto);
 
@@ -39,6 +43,38 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
 
         //保存套餐和菜品的关联信息，操作setmeal_dish，执行insert操作
         setmealDishService.saveBatch(setmealDishes);
+    }
+
+
+    /**
+     * 删除套餐，同时需要删除套餐和菜品的关联数据
+     * @param ids
+     */
+    @Override
+    @Transactional
+    public void removeWithDish(List<Long> ids) {
+        //select count(*) from setmeal where id in (1,2,3) and status = 1
+        //查询套餐状态，确定是否可用删除
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.in(Setmeal::getId, ids);
+        queryWrapper.eq(Setmeal::getStatus,1);
+
+        long count = this.count(queryWrapper);
+        if(count>0){
+            //如果不能删除，抛出一个业务异常
+            throw new CustomException("套餐正在售卖中，不能删除");
+        }
+
+        //如果可以删除，先删除套餐表中的数据---setmeal
+        this.removeByIds(ids);
+
+        //delete from setmeal_dish where setmeal_id in (1,2,3)
+        LambdaQueryWrapper<SetmealDish> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.in(SetmealDish::getSetmealId, ids);
+        //删除关系表中的数据---setmeal_dish
+        setmealDishService.remove(lambdaQueryWrapper);
+
+
 
     }
 }
